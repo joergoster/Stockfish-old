11baf47c4ec5561e797573c23d50b9ae4cce5b7d
joergoster <osterj165@googlemail.com>
2014-07-29 18:13:04 +0200

    Add 2-fold/3-fold repetition fix. bench: 7972140

--- src/search.cpp
+++ src/search.cpp
@@ -42,6 +42,7 @@ namespace Search {
   LimitsType Limits;
   std::vector<RootMove> RootMoves;
   Position RootPos;
+  int RootPly;
   Time::point SearchTime;
   StateStackPtr SetupStates;
 }
@@ -179,7 +180,8 @@ uint64_t Search::perft(Position& pos, Depth depth) {
 
 void Search::think() {
 
-  TimeMgr.init(Limits, RootPos.game_ply(), RootPos.side_to_move());
+  RootPly = RootPos.game_ply();
+  TimeMgr.init(Limits, RootPly, RootPos.side_to_move());
 
   int cf = Options["Contempt Factor"] * PawnValueEg / 100; // From centipawns
   DrawValue[ RootPos.side_to_move()] = VALUE_DRAW - Value(cf);
