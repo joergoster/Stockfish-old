92adff7f710cf2475fe5a9e339f68c855e67b227
joergoster <osterj165@googlemail.com>
2014-10-16 16:39:00 +0200

    Widen aspiration window in SMP search. Take 1. This widens the aspiration window with each additional Thread.

--- src/search.cpp
+++ src/search.cpp
@@ -282,7 +282,7 @@ namespace {
             // Reset aspiration window starting size
             if (depth >= 5)
             {
-                delta = Value(16);
+                delta = Value(8) + int(Threads.size() * 8);
                 alpha = std::max(RootMoves[PVIdx].prevScore - delta,-VALUE_INFINITE);
                 beta  = std::min(RootMoves[PVIdx].prevScore + delta, VALUE_INFINITE);
             }
