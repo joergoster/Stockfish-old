a3671e9e23e839f706065b01a508b2766eab2b14
joergoster <osterj165@googlemail.com>
2016-02-03 10:45:54 +0100

    New pawn, rook and queen values obtained from SPSA tuning. bench: 7470879

--- src/material.cpp
+++ src/material.cpp
@@ -32,28 +32,28 @@ namespace {
   // Polynomial material imbalance parameters
 
   //                      pair  pawn knight bishop rook queen
-  const int Linear[6] = { 1667, -168, -1027, -166,  238, -138 };
+  const int Linear[6] = { 1667, -196, -1027, -166,  305, -187 };
 
   const int QuadraticOurs[][PIECE_TYPE_NB] = {
     //            OUR PIECES
     // pair pawn knight bishop rook queen
     {   0                               }, // Bishop pair
-    {  40,    2                         }, // Pawn
-    {  32,  255,  -3                    }, // Knight      OUR PIECES
-    {   0,  104,   4,    0              }, // Bishop
-    { -26,   -2,  47,   105,  -149      }, // Rook
-    {-185,   24, 122,   137,  -134,   0 }  // Queen
+    {   0,   12                         }, // Pawn
+    {  32,  255,   -3                   }, // Knight      OUR PIECES
+    {   0,  104,    4,    0             }, // Bishop
+    {   0,  -24,   15,   31,  -214      }, // Rook
+    {   0,  -40,  103,  177,  -137,  -4 }  // Queen
   };
 
   const int QuadraticTheirs[][PIECE_TYPE_NB] = {
     //           THEIR PIECES
     // pair pawn knight bishop rook queen
     {   0                               }, // Bishop pair
-    {  36,    0                         }, // Pawn
-    {   9,   63,   0                    }, // Knight      OUR PIECES
-    {  59,   65,  42,     0             }, // Bishop
-    {  46,   39,  24,   -24,    0       }, // Rook
-    { 101,  100, -37,   141,  268,    0 }  // Queen
+    {   0,    0                         }, // Pawn
+    {   9,   63,    0                   }, // Knight      OUR PIECES
+    {  59,   65,   42,    0             }, // Bishop
+    {   0,  117,  -86,  -23,    0       }, // Rook
+    {   0,   88,   -1,  148,  366,    0 }  // Queen
   };
 
   // Endgame evaluation and scaling functions are accessed directly and not through
