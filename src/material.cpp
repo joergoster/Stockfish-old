ff9d6de636ef83c7594c270ea8b9b9858f5003da
joergoster <osterj165@googlemail.com>
2016-09-06 21:20:39 +0200

    A combo of 3 tuning patches. bench: 7001821

--- src/material.cpp
+++ src/material.cpp
@@ -34,7 +34,7 @@ namespace {
   const int QuadraticOurs[][PIECE_TYPE_NB] = {
     //            OUR PIECES
     // pair pawn knight bishop rook queen
-    {1667                               }, // Bishop pair
+    {1812                               }, // Bishop pair
     {  40,    2                         }, // Pawn
     {  32,  255,  -3                    }, // Knight      OUR PIECES
     {   0,  104,   4,    0              }, // Bishop
