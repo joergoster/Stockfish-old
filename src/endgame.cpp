c922722ce7ec70ba9c058ab38eb0057b3214b57a
joergoster <osterj165@googlemail.com>
2016-10-17 12:07:15 +0200

    Add win rule for 3 or more knights to KXK. Also remove the now redundant check for opposite bishops.
    bench unchanged.

--- src/endgame.cpp
+++ src/endgame.cpp
@@ -172,8 +172,8 @@ Value Endgame<KXK>::operator()(const Position& pos) const {
   if (   pos.count<QUEEN>(strongSide)
       || pos.count<ROOK>(strongSide)
       ||(pos.count<BISHOP>(strongSide) && pos.count<KNIGHT>(strongSide))
-      ||(pos.count<BISHOP>(strongSide) > 1 && opposite_colors(pos.squares<BISHOP>(strongSide)[0],
-                                                              pos.squares<BISHOP>(strongSide)[1])))
+      || pos.count<BISHOP>(strongSide) > 1
+      || pos.count<KNIGHT>(strongSide) > 2)
       result = std::min(result + VALUE_KNOWN_WIN, VALUE_MATE_IN_MAX_PLY - 1);
 
   return strongSide == pos.side_to_move() ? result : -result;
