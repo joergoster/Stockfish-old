d096a09bb01e8f63da2a553ece38aa6542470a6a
joergoster <osterj165@googlemail.com>
2016-12-23 19:30:47 +0100

    Revert "Move scoring of unstoppable into passed pawn eval."
    This reverts commit 55405f6b789c086f9ed19deaae5519ec90251a63.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -208,7 +208,7 @@ namespace {
   const Score WeakQueen           = S(50, 10);
   const Score Hanging             = S(48, 27);
   const Score ThreatByPawnPush    = S(38, 22);
-  const Score Unstoppable         = S( 0,196);
+  const Score Unstoppable         = S( 0, 45);
   const Score PawnlessFlank       = S(20, 80);
   const Score HinderPassedPawn    = S( 7,  0);
   const Score ThreatByRank        = S(16,  3);
@@ -636,12 +636,6 @@ namespace {
 
     b = ei.pi->passed_pawns(Us);
 
-    // Bonus for having a passed pawn in very late endgame
-    if (   b
-        && pos.non_pawn_material(Us  ) <= KnightValueMg
-        && pos.non_pawn_material(Them) <= KnightValueMg)
-        score += Unstoppable;
-
     while (b)
     {
         Square s = pop_lsb(&b);
@@ -877,6 +871,16 @@ Value Eval::evaluate(const Position& pos) {
   // Evaluate passed pawns, we need full attack information including king
   score +=  evaluate_passed_pawns<WHITE, DoTrace>(pos, ei)
           - evaluate_passed_pawns<BLACK, DoTrace>(pos, ei);
+
+  // If both sides have only pawns, score for potential unstoppable pawns
+  if (!pos.non_pawn_material(WHITE) && !pos.non_pawn_material(BLACK))
+  {
+      if (ei.pi->passed_pawns(WHITE))
+          score += Unstoppable;
+
+      if (ei.pi->passed_pawns(BLACK))
+          score -= Unstoppable;
+  }
 
   // Evaluate space for both sides, only during opening
   if (pos.non_pawn_material(WHITE) + pos.non_pawn_material(BLACK) >= 12317)
