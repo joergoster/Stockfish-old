8a6b92dac832149b6e3ad8c93685ed4c91eeb71d
joergoster <osterj165@googlemail.com>
2017-03-24 12:23:37 +0100

    Update comment in LMR step. No functional change.

--- src/search.cpp
+++ src/search.cpp
@@ -974,7 +974,9 @@ moves_loop: // When in check and at SpNode search starts from here
                                     [pos.piece_on(to_sq(move))][to_sq(move)] > VALUE_ZERO)
               ss->reduction = std::max(DEPTH_ZERO, ss->reduction - ONE_PLY);
 
-          // Decrease reduction for moves that escape a capture
+          // Decrease reduction for moves that escape a capture. Filter out castling
+          // moves because are coded as "king captures rook" and break make_move().
+          // Also use see() instead of see_sign() because destination square is empty.
           if (   ss->reduction
               && type_of(move) == NORMAL
               && type_of(pos.piece_on(to_sq(move))) != PAWN
