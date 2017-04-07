30da0d19ad8330463b831e2e51d24601de263a75
joergoster <osterj165@googlemail.com>
2017-04-07 19:34:21 +0200

    Update values. Bench: 6128125

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -138,8 +138,8 @@ namespace {
   // supported by a pawn. If the minor piece occupies an outpost square
   // then score is doubled.
   const Score Outpost[][2] = {
-    { S(21, 9), S(61, 3) }, // Knight
-    { S( 5,20), S(28,42) }  // Bishop
+    { S(18, 5), S(33, 9) }, // Knight
+    { S( 7, 2), S(12, 4) }  // Bishop
   };
 
   // RookOnFile[semiopen/open] contains bonuses for each rook when there is no
