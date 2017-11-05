3cadb5857558120eaa094c92cb44959869eeefe0
joergoster <osterj165@googlemail.com>
2017-11-05 17:19:32 +0100

    Update. Bench: 4640119

--- src/search.cpp
+++ src/search.cpp
@@ -687,7 +687,7 @@ namespace {
     if (   !PvNode
         &&  eval >= beta
         && (ss->staticEval >= beta - 35 * (depth / ONE_PLY - 6) || depth >= 13 * ONE_PLY)
-        &&  thisThread->maxPly + 5 * ONE_PLY > thisThread->rootDepth
+        &&  thisThread->selDepth + 5 > thisThread->rootDepth / ONE_PLY
         && !(depth > 12 * ONE_PLY && MoveList<LEGAL>(pos).size() < 4)
         &&  pos.non_pawn_material(pos.side_to_move()) > (depth > 12 * ONE_PLY) * BishopValueMg)
     {
