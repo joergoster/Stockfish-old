9b26803bd3b1bbea68dbdb0b1c123d77de37fe16
joergoster <osterj165@googlemail.com>
2017-11-15 12:00:41 +0100

    Take 2. Bench: 5187332

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -756,9 +756,8 @@ namespace {
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int initiative =   8 * (pe->pawn_asymmetry() + kingDistance - 17)
-                    + 12 * pos.count<PAWN>() + 16 * bothFlanks
-                    +  6 * (pos.count<ALL_PIECES>() - pos.count<PAWN>());
+    int initiative =  8 * (pe->pawn_asymmetry() + kingDistance - 17)
+                    + 6 * (pos.count<ALL_PIECES>() - 2) + 16 * bothFlanks;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
