3acf1e2a0deffc9a074214f27c56d49785b9f566
joergoster <osterj165@googlemail.com>
2017-11-26 11:45:35 +0100

    Always compute material imbalance evaluation in case there is no specialized evaluation function available. And also check for 'real' pair of bishops.
    Passed sprt[-5, 0] non-regression test, tc 30+0.3
    Elo difference: 7.94 +/- 8.25
    SPRT: llr 2.95, lbound -2.94, ubound 2.94 - H1 was accepted
    
    Bench: 8817946

--- src/position.h
+++ src/position.h
@@ -141,6 +141,7 @@ public:
   // Piece specific
   bool pawn_passed(Color c, Square s) const;
   bool opposite_bishops() const;
+  bool bishop_pair(Color c) const;
 
   // Doing and undoing moves
   void do_move(Move m, StateInfo& st, bool givesCheck);
@@ -365,6 +366,12 @@ inline bool Position::opposite_bishops() const {
   return   pieceCount[WHITE][BISHOP] == 1
         && pieceCount[BLACK][BISHOP] == 1
         && opposite_colors(square<BISHOP>(WHITE), square<BISHOP>(BLACK));
+}
+
+inline bool Position::bishop_pair(Color c) const {
+  return   pieceCount[c][BISHOP] > 1
+        && byColorBB[c] & byTypeBB[BISHOP] &  DarkSquares
+        && byColorBB[c] & byTypeBB[BISHOP] & ~DarkSquares;
 }
 
 inline bool Position::is_chess960() const {
