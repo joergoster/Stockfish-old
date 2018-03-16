d901d6bac412209362476352d2b82bbd2729fd66
joergoster <osterj165@googlemail.com>
2018-03-16 19:11:26 +0100

    Tweak complexity factors. Try some early values.
    Bench: 5353870

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -762,11 +762,11 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * outflanking
-                    +  8 * pe->pawn_asymmetry()
-                    + 12 * pos.count<PAWN>()
-                    + 16 * pawnsOnBothFlanks
-                    + 48 * !pos.non_pawn_material()
-                    -136 ;
+                    + 12 * pe->pawn_asymmetry()
+                    + 19 * pos.count<PAWN>()
+                    + 30 * pawnsOnBothFlanks
+                    + 50 * !pos.non_pawn_material()
+                    -191 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
