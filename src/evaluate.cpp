93afebdb7f322385da3f38579b2b4bd970cc3c00
joergoster <osterj165@googlemail.com>
2018-03-21 19:45:34 +0100

    Take 3. Bench: 5550578

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -762,12 +762,12 @@ namespace {
                             && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int complexity =   6 * outflanking
-                    +  8 * pe->pawn_asymmetry()
-                    + 14 * pos.count<PAWN>()
-                    + 19 * pawnsOnBothFlanks
-                    + 53 * !pos.non_pawn_material()
-                    -135;
+    int complexity =    8 * outflanking
+                    +  13 * pe->pawn_asymmetry()
+                    +  12 * pos.count<PAWN>()
+                    +  47 * pawnsOnBothFlanks
+                    +  71 * !pos.non_pawn_material()
+                    - 142;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
