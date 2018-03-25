a331c30b6ae0a900c29cdca6da216f3179aa2f31
joergoster <osterj165@googlemail.com>
2018-03-25 17:58:17 +0200

    Some restructuring. And also return beta instead of VALUE_DRAW when reaching MAX_PLY and in check in search().
    Bench unchanged.

--- src/search.cpp
+++ src/search.cpp
@@ -528,7 +528,7 @@ namespace {
     bestValue = -VALUE_INFINITE;
     maxValue = VALUE_INFINITE;
 
-    // Check for the available remaining time
+    // Step 2. Check for the available remaining time
     if (thisThread == Threads.main())
         static_cast<MainThread*>(thisThread)->check_time();
 
@@ -536,22 +536,30 @@ namespace {
     if (PvNode && thisThread->selDepth < ss->ply)
         thisThread->selDepth = ss->ply;
 
+    // Step 3. Check for immediate return conditions
     if (!rootNode)
     {
-        // Step 2. Check for aborted search and immediate draw
-        if (   Threads.stop.load(std::memory_order_relaxed)
-            || pos.is_draw(ss->ply)
-            || ss->ply >= MAX_PLY)
-            return (ss->ply >= MAX_PLY && !inCheck) ? evaluate(pos) : VALUE_DRAW;
+        // Step 3a. Check for aborted search
+        if (Threads.stop.load(std::memory_order_relaxed))
+            return VALUE_ZERO;
 
-        // Step 3. Mate distance pruning. Even if we mate at the next move our score
+        // Step 3b. Check for immediate draw
+        if (pos.is_draw(ss->ply))
+            return VALUE_DRAW;
+
+        // Step 3c. Check for maximum ply reached
+        if (ss->ply >= MAX_PLY)
+            return !inCheck ? evaluate(pos) : beta;
+
+        // Step 3d. Mate distance pruning. Even if we mate at the next move our score
         // would be at best mate_in(ss->ply+1), but if alpha is already bigger because
         // a shorter mate was found upward in the tree then there is no need to search
         // because we will never beat the current alpha. Same logic but with reversed
         // signs applies also in the opposite condition of being mated instead of giving
         // mate. In this case return a fail-high score.
         alpha = std::max(mated_in(ss->ply), alpha);
-        beta = std::min(mate_in(ss->ply+1), beta);
+        beta  = std::min(mate_in(ss->ply+1), beta);
+
         if (alpha >= beta)
             return alpha;
     }
