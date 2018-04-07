b4b155eb9b2fd31e658e6eb3da0d75e5fe4ae67e
joergoster <osterj165@googlemail.com>
2018-04-07 10:59:52 +0200

    Restore the old order of evaluating pieces. Bench: 6385490

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -865,17 +865,17 @@ namespace {
     initialize<BLACK>();
 
     // Pieces should be evaluated first (populate attack tables)
-    score +=  pieces<WHITE, KNIGHT>() - pieces<BLACK, KNIGHT>()
-            + pieces<WHITE, BISHOP>() - pieces<BLACK, BISHOP>()
-            + pieces<WHITE, ROOK  >() - pieces<BLACK, ROOK  >()
-            + pieces<WHITE, QUEEN >() - pieces<BLACK, QUEEN >();
+    score += pieces<WHITE, KNIGHT>() - pieces<BLACK, KNIGHT>();
+    score += pieces<WHITE, BISHOP>() - pieces<BLACK, BISHOP>();
+    score += pieces<WHITE, ROOK  >() - pieces<BLACK, ROOK  >();
+    score += pieces<WHITE, QUEEN >() - pieces<BLACK, QUEEN >();
 
     score += mobility[WHITE] - mobility[BLACK];
 
-    score +=  king<   WHITE>() - king<   BLACK>()
-            + threats<WHITE>() - threats<BLACK>()
-            + passed< WHITE>() - passed< BLACK>()
-            + space<  WHITE>() - space<  BLACK>();
+    score += king<   WHITE>() - king<   BLACK>();
+    score += threats<WHITE>() - threats<BLACK>();
+    score += passed< WHITE>() - passed< BLACK>();
+    score += space<  WHITE>() - space<  BLACK>();
 
     score += initiative(eg_value(score));
 
