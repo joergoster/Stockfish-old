632bd9f4c010c1920d7002f5717a331af2ee2f24
joergoster <osterj165@googlemail.com>
2018-05-03 18:02:43 +0200

    Always apply analysis contempt setting when not in game-play mode.
    Gives the user more freedom to decide, what contempt setting is used
    when e. g. searching for mate, fixed nodes, depth or movetime,
    and not only in analysis mode.
    
    Since bench searches to a fixed depth, and default setting is 'Off',
    this looks like a functional change. But with Analysis Contempt set to 'Both',
    bench signature remains the same.
    
    Bench: 7813890

--- src/search.cpp
+++ src/search.cpp
@@ -311,16 +311,16 @@ void Thread::search() {
   multiPV = std::min((size_t)Options["MultiPV"], rootMoves.size());
 
   int baseCt = Options["Contempt"] * PawnValueEg / 100; // From centipawns
-  int ct = 1; // Factor for changing sign of contempt when analyzing
+  int ct = 1; // Used for changing sign of contempt when analyzing
 
   // In analysis mode, adjust contempt in accordance with user preference
-  if (Limits.infinite)
+  if (!Limits.use_time_management())
       ct =  Options["Analysis Contempt"] == "Off" ?  0
           : Options["Analysis Contempt"] == "White" && us == BLACK ? -ct
           : Options["Analysis Contempt"] == "Black" && us == WHITE ? -ct
           : ct; // contempt remains with the side to move
 
-  // In evaluate.cpp the evaluation is from the white point of view
+  // In evaluate.cpp the evaluation is from White's point of view
   baseCt *= ct;
   contempt = (us == WHITE ?  make_score(baseCt, baseCt / 2)
                           : -make_score(baseCt, baseCt / 2));
