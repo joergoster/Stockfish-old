c51b0404c65046914a4804bc631e54ccc292d86a
joergoster <osterj165@googlemail.com>
2018-10-30 10:10:23 +0100

    No contempt during opening phase. Bench: 3528468

--- src/search.cpp
+++ src/search.cpp
@@ -332,6 +332,10 @@ void Thread::search() {
 
   int ct = int(Options["Contempt"]) * PawnValueEg / 100; // From centipawns
 
+  // No static contempt during opening phase
+  if (rootPos.count<PAWN>() > 12)
+      ct = 0;
+
   // In analysis mode, adjust contempt in accordance with user preference
   if (Limits.infinite || Options["UCI_AnalyseMode"])
       ct =  Options["Analysis Contempt"] == "Off"  ? 0
