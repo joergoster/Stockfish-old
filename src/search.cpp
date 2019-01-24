3ccf4fe80db022f9c320c31fca583e1d048e4c7e
joergoster <osterj165@googlemail.com>
2019-01-24 16:50:34 +0100

    Decrease null reduction with only 1 minor piece left. Bench: 3741100

--- src/search.cpp
+++ src/search.cpp
@@ -791,6 +791,10 @@ namespace {
         // Null move dynamic reduction based on depth and value
         Depth R = ((823 + 67 * depth / ONE_PLY) / 256 + std::min(int(eval - beta) / 200, 3)) * ONE_PLY;
 
+        // Decrease reduction with only one minor piece left
+        if (pos.non_pawn_material(us) <= BishopValueMg)
+            R -= ONE_PLY;
+ 
         ss->currentMove = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[NO_PIECE][0];
 
