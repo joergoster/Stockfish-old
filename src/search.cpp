f6a8560e636d06bedb7b0ff5157fa608d836c454
joergoster <osterj165@googlemail.com>
2019-06-17 17:46:59 +0200

    Take 3. Bench: 3543925

--- src/search.cpp
+++ src/search.cpp
@@ -1036,9 +1036,9 @@ moves_loop: // When in check, search starts from here
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
               // hence break make_move(). (~5 Elo)
-              if (    type_of(move) == NORMAL
-                  && !pos.see_ge(make_move(to_sq(move), from_sq(move))))
-                  r -= (cutNode ? 1 : 2) * ONE_PLY;
+              else if (    type_of(move) == NORMAL
+                       && !pos.see_ge(make_move(to_sq(move), from_sq(move))))
+                  r -= (moveCount > 5 ? 1 : 2) * ONE_PLY;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
