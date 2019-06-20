87836b02843cd78f92d189e1a48520b5184dc8c8
joergoster <osterj165@googlemail.com>
2019-06-20 12:19:11 +0200

    Fix an oversight. Bench: 5279212

--- src/search.cpp
+++ src/search.cpp
@@ -900,7 +900,7 @@ moves_loop: // When in check, search starts from here
       ss->moveCount = ++moveCount;
 
       if (rootNode && thisThread == Threads.main() && Time.elapsed() > 3000)
-          sync_cout << "info depth " << depth / ONE_PLY
+          sync_cout << "info depth " << thisThread->rootDepth / ONE_PLY
                     << " currmove " << UCI::move(move, pos.is_chess960())
                     << " currmovenumber " << moveCount + thisThread->pvIdx << sync_endl;
       if (PvNode)
