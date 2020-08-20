bed565edb73e1d56c8a3a96e94047b1a792ad1d6
mstembera <MissingEmail@email>
2020-08-20 16:59:27 -0700

    Support VNNI on 256bit vectors
    due to downclocking on current chips (tested up to cascade lake)
    supporting avx512 and vnni512, it is better to use avx2 or vnni256
    in multithreaded (in particular hyperthreaded) engine use.
    In single threaded use, the picture is different.
    
    gcc compilation for vnni256 requires a toolchain for gcc >= 9.
    
    closes https://github.com/official-stockfish/Stockfish/pull/3038
    
    No functional change

--- src/nnue/layers/affine_transform.h
+++ src/nnue/layers/affine_transform.h
@@ -90,8 +90,10 @@ namespace Eval::NNUE::Layers {
 
   #elif defined(USE_AVX2)
       constexpr IndexType kNumChunks = kPaddedInputDimensions / kSimdWidth;
-      const __m256i kOnes = _mm256_set1_epi16(1);
       const auto input_vector = reinterpret_cast<const __m256i*>(input);
+  #if !defined(USE_VNNI)
+      const __m256i kOnes = _mm256_set1_epi16(1);
+  #endif
 
   #elif defined(USE_SSE2)
       constexpr IndexType kNumChunks = kPaddedInputDimensions / kSimdWidth;
@@ -161,11 +163,15 @@ namespace Eval::NNUE::Layers {
 
           for (IndexType j = 0; j < kNumChunks; ++j)
           {
+  #if defined(USE_VNNI)
+              sum = _mm256_dpbusd_epi32(sum, _mm256_loadA_si256(&input_vector[j]), _mm256_load_si256(&row[j]));
+  #else
               __m256i product = _mm256_maddubs_epi16(_mm256_loadA_si256(&input_vector[j]),
                                                      _mm256_load_si256(&row[j]));
               product = _mm256_madd_epi16(product, kOnes);
 
               sum = _mm256_add_epi32(sum, product);
+  #endif
           }
 
           __m128i sum128 = _mm_add_epi32(_mm256_castsi256_si128(sum),
