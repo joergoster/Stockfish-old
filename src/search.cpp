71ec3256707c29b55eeff4802c9e6906eb67fc85
J. Oster <osterj165@googlemail.com>
2020-11-01 18:49:23 +0100

    This check seems more appropriate. Bench: 3625915

--- src/search.cpp
+++ src/search.cpp
@@ -1565,7 +1565,7 @@ moves_loop: // When in check, search starts from here
 
       // CounterMove based pruning
       if (  !captureOrPromotion
-          && bestValue > VALUE_TB_LOSS_IN_MAX_PLY
+          && bestValue > VALUE_MATED_IN_MAX_PLY
           && (*contHist[0])[pos.moved_piece(move)][to_sq(move)] < CounterMovePruneThreshold
           && (*contHist[1])[pos.moved_piece(move)][to_sq(move)] < CounterMovePruneThreshold)
           continue;
