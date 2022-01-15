6bf412041de72e232862b32293fb75350c44bfc3
J. Oster <osterj165@googlemail.com>
2022-01-15 18:18:52 +0100

    Simplify limiting extensions. Bench: 4550528

--- src/thread.h
+++ src/thread.h
@@ -60,15 +60,10 @@ public:
   Pawns::Table pawnsTable;
   Material::Table materialTable;
   size_t pvIdx, pvLast;
-  RunningAverage doubleExtensionAverage[COLOR_NB];
-  uint64_t nodesLastExplosive;
-  uint64_t nodesLastNormal;
   std::atomic<uint64_t> nodes, tbHits, bestMoveChanges;
-  Value bestValue;
   int selDepth, nmpMinPly;
   Color nmpColor;
-  ExplosionState state;
-  Value optimism[COLOR_NB];
+  Value bestValue, optimism[COLOR_NB];
 
   Position rootPos;
   StateInfo rootState;
