6757df23635d04f1d5f8181824cc959a01a5fe5b
J. Oster <osterj165@googlemail.com>
2022-03-29 19:26:57 +0200

    Take 2. Bench: 6770646

--- src/search.cpp
+++ src/search.cpp
@@ -1185,10 +1185,10 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                  ? 0
-                       : PvNode && depth > 4      ? 1
                        : cutNode && moveCount < 9 ? 1
                        : moveCount < 3            ? 2
                        : moveCount < 5            ? 1
+                       : PvNode && depth > 4      ? 1
                        :                            0;
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
